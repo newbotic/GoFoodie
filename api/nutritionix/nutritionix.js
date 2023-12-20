f92d663dd44d6ded3574e3d027b90531566ec84f
newbotic <like_plus@yahoo.es>
2023-12-20 18:07:51 +0000

    request foods

--- api/nutritionix/nutritionix.js
+++ api/nutritionix/nutritionix.js
@@ -1,7 +1,23 @@
 
 // https://developer.syndigo.com/docs/natural-language-for-nutrients
 
-function naturalLanguageForNutrients(){
+
+$(document).ready(function() {
+
+
+	function generateInput(type, name, placeholder) {
+	  var inputElement = $('<input>').attr({
+		'type': type,
+		'name': name,
+		'placeholder': placeholder
+	  });
+
+	  return inputElement;
+	}
+
+// ---------------------------------
+
+function naturalLanguageForNutrients(query){
 var url = 'https://trackapi.nutritionix.com/v2/natural/nutrients';
 var headers = {
   'Content-Type': 'application/json',
@@ -10,7 +26,7 @@ var headers = {
 };
 
 var data = {
-  query: 'apple',
+  query: query
 
 };
 
@@ -21,42 +37,100 @@ fetch(url, {
 })
   .then(response => response.json())
   .then(data => console.log(data))
+    // .then(data => renderData())
   .catch(error => console.error('Error:', error));
 };
 
+//----------------------------------------
 
-// naturalLanguageForNutrients();
+var form = $('<form>');
+
+      var queryInput = generateInput('text', 'query', 'Enter your query');
+      var submitButton = $('<button>').text('Search');
+
+      form.append(queryInput);
+      form.append(submitButton);
+
+      // Append the form to an element with id 'app'
+      $('#foodDiv').append(form);
+
+
+// ----------------------------------------------------------
+function renderData(data) {
+	let foods ='';
+	var resultDiv = $('<div>').attr('id', 'result');
+
+	if (data && data.foods && data.foods.length > 0) {
+	  resultDiv.append($('<p>').text('Calories: ' + data));
+
+	} else {
+	  resultDiv.append($('<p>').text('No results found.'));
+	}
+  
+	// Append the result div to an element with id 'app'
+	$('#foodDiv').append(resultDiv);
+  }
+
+
+
+	// resultDiv.append($('<p>').text('Protein: ' + data.protein));
+	// resultDiv.append($('<p>').text('Carbs: ' + data.carbs));
+	// resultDiv.append($('<p>').text('Fat: ' + data.fat));
+
+  
+
+      // Attach event listener to the form
+
+      form.on('submit', function(event) {
+        event.preventDefault(); // Prevents the default form submission behavior
+        var enteredQuery = queryInput.val();
+        naturalLanguageForNutrients(enteredQuery);
+      });
+
+    });
+
 
 // ---------------------------------------------------------------------------------------------
 
 
-https://developer.syndigo.com/docs/instant-endpoint
 
-function instantEndpoint() {
-	var url = 'https://trackapi.nutritionix.com/v2/search/instant/?query=hamburger';
-	var headers = {
-	  'Content-Type': 'application/json',
-	  'x-app-id': '3231e20a',
-	  'x-app-key': '1099567b2d68a5ca3dc414335fb76cfc' 
-	};
+
+
+
+
+
+
+
+
+
+
+// https://developer.syndigo.com/docs/instant-endpoint
+
+// function instantEndpoint() {
+// 	var url = 'https://trackapi.nutritionix.com/v2/search/instant/?query=hamburger';
+// 	var headers = {
+// 	  'Content-Type': 'application/json',
+// 	  'x-app-id': '3231e20a',
+// 	  'x-app-key': '1099567b2d68a5ca3dc414335fb76cfc' 
+// 	};
   
-	fetch(url, {
-	  method: 'GET',
-	  headers: headers
-	})
-	  .then(response => {
-		if (!response.ok) {
-		  throw new Error('Network response was not ok');
-		}
-		return response.json();
-	  })
-	  .then(data => {
-		console.log(data);
-	  })
-	  .catch(error => {
-		console.error('Error:', error);
-	  });
-  }
+// 	fetch(url, {
+// 	  method: 'GET',
+// 	  headers: headers
+// 	})
+// 	  .then(response => {
+// 		if (!response.ok) {
+// 		  throw new Error('Network response was not ok');
+// 		}
+// 		return response.json();
+// 	  })
+// 	  .then(data => {
+// 		console.log(data);
+// 	  })
+// 	  .catch(error => {
+// 		console.error('Error:', error);
+// 	  });
+//   }
   
 //   instantEndpoint();
 
@@ -66,34 +140,34 @@ function instantEndpoint() {
   
 //https://developer.syndigo.com/docs/search-item-endpoint
 
-function searchItemEndpoint() {
-	var url = 'https://trackapi.nutritionix.com/v2/search/item/?upc=49000000450';
-	var headers = {
-	  'Content-Type': 'application/x-www-form-urlencoded',
-	  'x-app-id': '3231e20a', 
-	  'x-app-key': '1099567b2d68a5ca3dc414335fb76cfc' 
-		};
+// function searchItemEndpoint() {
+// 	var url = 'https://trackapi.nutritionix.com/v2/search/item/?upc=49000000450';
+// 	var headers = {
+// 	  'Content-Type': 'application/x-www-form-urlencoded',
+// 	  'x-app-id': '3231e20a', 
+// 	  'x-app-key': '1099567b2d68a5ca3dc414335fb76cfc' 
+// 		};
   
-	fetch(url, {
-	  method: 'GET',
-	  headers: headers
-	})
-	  .then(response => {
-		if (!response.ok) {
-		  throw new Error('Network response was not ok');
-		}
-		return response.json();
-	  })
-	  .then(data => {
-		console.log(data);
-	  })
-	  .catch(error => {
-		console.error('Error:', error);
-	  });
-  }
+// 	fetch(url, {
+// 	  method: 'GET',
+// 	  headers: headers
+// 	})
+// 	  .then(response => {
+// 		if (!response.ok) {
+// 		  throw new Error('Network response was not ok');
+// 		}
+// 		return response.json();
+// 	  })
+// 	  .then(data => {
+// 		console.log(data);
+// 	  })
+// 	  .catch(error => {
+// 		console.error('Error:', error);
+// 	  });
+//   }
   
 //   searchItemEndpoint();
   
 
 
-// ---------------------------------------------------------------------------------------------------------------------
+// // ---------------------------------------------------------------------------------------------------------------------
